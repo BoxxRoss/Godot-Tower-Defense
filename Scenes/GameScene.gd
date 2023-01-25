785d2a63b0c2f3ea5425fd157aa4f688d0186db9
William Burns <wsbsc5@yahoo.com>
2023-01-25 10:13:38 -0800

    finished the drag UI,you can place towers now

--- Scenes/GameScene.gd
+++ Scenes/GameScene.gd
@@ -14,10 +14,15 @@ func _ready():
 		i.connect("pressed", self, "initiate_build_mode", [i.get_name()])
 	
 func _process(delta):
-	pass
+	if build_mode:
+		update_tower_preview()
 	
 func _unhandled_input(event):
-	pass
+	if event.is_action_pressed("cancel") and build_mode == true:
+		cancel_build_mode()
+	if event.is_action_released("ui_accept") and build_mode == true:
+		verify_and_build()
+		cancel_build_mode()
 	
 func initiate_build_mode(tower_type):
 	build_type = tower_type + "T1"
@@ -25,10 +30,25 @@ func initiate_build_mode(tower_type):
 	get_node("UI").set_tower_preview(build_type, get_global_mouse_position())
 	
 func update_tower_preview():
-	pass
+	var mouse_position = get_global_mouse_position()
+	var current_tile = map_node.get_node("TowerExclusion").world_to_map(mouse_position)
+	var tile_position = map_node.get_node("TowerExclusion").map_to_world(current_tile)
+	
+	if map_node.get_node("TowerExclusion").get_cellv(current_tile) == -1:
+		get_node("UI").update_tower_preview(tile_position, "ad54ff3c")
+		build_valid = true
+		build_location = tile_position
+	else:
+		get_node("UI").update_tower_preview(tile_position, "adff4545")
+		build_valid = false
 	
 func cancel_build_mode():
-	pass
+	build_mode = false
+	build_valid = false
+	get_node("UI/TowerPreview").queue_free()
 	
 func verify_and_build():
-	pass	
+	if build_valid:
+		var new_tower = load("res://SpriteAssets/Towers/" + build_type +".tscn").instance()
+		new_tower.position = build_location
+		map_node.get_node("Turrets").add_child(new_tower, true)
